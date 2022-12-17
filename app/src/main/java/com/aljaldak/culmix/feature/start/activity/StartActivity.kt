e788141fc797454bcdaac2b8ed092e3d30b84d4b
Junsu <krpjs0508@gmail.com>
2022-12-18 03:34:20 +0900

    feat :: StartActivity 버튼 로직

--- app/src/main/java/com/aljaldak/culmix/feature/start/activity/StartActivity.kt
+++ app/src/main/java/com/aljaldak/culmix/feature/start/activity/StartActivity.kt
@@ -1,10 +1,13 @@
 package com.aljaldak.culmix.feature.start.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.aljaldak.culmix.R
 import com.aljaldak.culmix.databinding.ActivityStartBinding
+import com.aljaldak.culmix.feature.login.activity.LoginActivity
+import com.aljaldak.culmix.feature.register.RegisterActivity
 
 class StartActivity : AppCompatActivity() {
 
@@ -18,7 +21,20 @@ class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.btnStartLogin.setOnClickListener {
-            
+            startActivity(
+                Intent(
+                    this,
+                    LoginActivity::class.java,
+                )
+            )
+        }
+        binding.btnStartRegister.setOnClickListener {
+            startActivity(
+                Intent(
+                    this,
+                    RegisterActivity::class.java,
+                )
+            )
         }
     }
 }
