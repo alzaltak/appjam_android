6bb33471e65653f9cd4d4a3c1884fd58bda355af
이현빈 <82383983+leehyeonbin@users.noreply.github.com>
2022-12-18 08:57:13 +0900

    feat : 안쓰는 코드 정리

--- app/src/main/java/com/aljaldak/culmix/feature/join/2_JoinAddressScreen.kt
+++ app/src/main/java/com/aljaldak/culmix/feature/join/2_JoinAddressScreen.kt
@@ -1,20 +1,8 @@
 package com.aljaldak.culmix.feature.join
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
