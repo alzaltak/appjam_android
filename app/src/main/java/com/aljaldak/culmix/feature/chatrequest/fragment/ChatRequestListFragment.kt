0eb43eb3636897b23078ff855f7fb1be852c1793
Junsu <krpjs0508@gmail.com>
2022-12-18 01:30:09 +0900

    feat :: 대화 신청 목록 UI 구성

---
+++ app/src/main/java/com/aljaldak/culmix/feature/chatrequest/fragment/ChatRequestListFragment.kt
@@ -0,0 +1,28 @@
+package com.aljaldak.culmix.feature.chatrequest.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.aljaldak.culmix.databinding.FragmentChatRequestListBinding
+
+class ChatRequestListFragment : Fragment() {
+
+    private lateinit var binding : FragmentChatRequestListBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentChatRequestListBinding.inflate(
+            inflater,
+            container,
+            false,
+        )
+        return binding.root
+    }
+
+
+}
