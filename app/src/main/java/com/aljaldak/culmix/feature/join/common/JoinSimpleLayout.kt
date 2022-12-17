fca031f3fe3c174a82ca613c990f57d22b1b30b8
limsaehyun <sh00710000@gmail.com>
2022-12-18 02:13:02 +0900

    feat: centor horizontal

--- app/src/main/java/com/aljaldak/culmix/feature/join/common/JoinSimpleLayout.kt
+++ app/src/main/java/com/aljaldak/culmix/feature/join/common/JoinSimpleLayout.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
@@ -22,7 +23,8 @@ fun JoinSimpleLayout(
 
         Column(
             modifier = Modifier
-                .padding(horizontal = 30.dp)
+                .padding(horizontal = 30.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             content()
         }
